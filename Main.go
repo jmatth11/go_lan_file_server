5d8cfe893c30aaa4e40cb14e684d51cfc5e76221
Joshua Matthews <jmatth93@gmail.com>
2017-04-04 23:03:03 -0500

    added image and base64 encoding of image to post request. changed FileData Data attribute to have []byte type to accept b64 input

--- Main.go
+++ Main.go
@@ -10,9 +10,13 @@ import (
 type FileData struct {
 	// change Data type to []byte when using base64
 	// []byte looks for base64 encoding otherwise throws error
-	Data    string
+	Data    []byte
 	Section int8
 	Name    string
+}
+
+func SpawnNewFile(fd FileData) {
+
 }
 
 // package mime/multipart. try using to help with large files
@@ -27,7 +31,7 @@ func PostFile(w http.ResponseWriter, req *http.Request) {
 	}
 	defer req.Body.Close()
 	fmt.Printf("File data, Name: %s Section: %d Data: %s", data.Name, data.Section, string(data.Data))
-	w.Write([]byte("File data recieved."))
+	w.Write([]byte("File data recieved.\n"))
 }
 
 func main() {
