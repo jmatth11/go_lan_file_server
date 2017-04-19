c151c2c6cd233fb417411565dda30dbfec449f8b
Joshua Matthews <jmatth93@gmail.com>
2017-04-18 20:53:43 -0500

    cleaned up some of the code and changed the signature of the GetFiles method

--- Main.go
+++ Main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 	"io/ioutil"
+	"strconv"
 )
 
 type FileData struct {
@@ -15,17 +17,25 @@ type FileData struct {
 	Name    string
 }
 
-// rename to ItemTypeList
-type FoldersList struct {
-	Folders []Folder
+type FileDataList struct {
+	Files []FileData
+	Error string
 }
 
-// rename to ItemType
+type FoldersList struct {
+	Folders []Folder
+	Error string
+}
+
 type Folder struct {
 	Name string
 	Count int
 }
 
+/**
+ * Method to create the file in the current dates folder.
+ * @param FileData
+ */
 func SpawnNewFile(fd FileData) {
 	path := CreateTodaysFolder()
 	file, err := os.Create(path + "\\" + fd.Name)
@@ -39,6 +49,10 @@ func SpawnNewFile(fd FileData) {
 	file.Close()
 }
 
+/**
+ * Method to create a folder with the current date as its name.
+ * @return string  The folder path
+ */
 func CreateTodaysFolder() string {
 	year, month, day := time.Now().Date()
 	name := fmt.Sprintf("Data\\%d-%d-%d", year, month, day)
@@ -49,7 +63,9 @@ func CreateTodaysFolder() string {
 	return name
 }
 
-
+/**
+ * Method to handle post request for a file to be saved to the server.
+ */
 func PostFile(w http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
 	var data FileData
@@ -59,10 +75,13 @@ func PostFile(w http.ResponseWriter, req *http.Request) {
 	}
 	defer req.Body.Close()
 	SpawnNewFile(data)
-	fmt.Printf("File data, Name: %s Data: %s", data.Name, string(data.Data))
+	log.Printf("File data, Name: %s", data.Name)
 	w.Write([]byte("File data recieved.\n"))
 }
 
+/**
+ * Method to retrieve the list of folder names in the Data path.
+ */
 func GetFolders(w http.ResponseWriter, req *http.Request) {
 	files, err := ioutil.ReadDir("Data\\.")
 	if err != nil {
@@ -84,16 +103,38 @@ func GetFolders(w http.ResponseWriter, req *http.Request) {
 	w.Write(b)
 }
 
+/**
+ * Method to accept a GET request for a specific folder in the Data path requesting files
+ * from start_index to end_index(exclusively).
+ */
 func GetFiles(w http.ResponseWriter, req *http.Request) {
 	 folder := req.URL.Query().Get("folder")
+	 start_index, _ := strconv.Atoi(req.URL.Query().Get("start_index"))
+	 end_index, _ := strconv.Atoi(req.URL.Query().Get("end_index"))
+	 if start_index > end_index {
+		 log.Fatal(fmt.Sprintf("ERROR: folder: %s, start_index: %d, end_index: %d", folder, start_index, end_index))
+		 err_files := FileDataList{Error:"ERROR: Start index is greater than End index"}
+		 b, err := json.Marshal(err_files)
+		 if err != nil {
+			 log.Fatal(err)
+		 }
+		 w.Write(b)
+		 return
+	 }
 	 files, err := ioutil.ReadDir("Data\\" + folder)
 	 if err != nil {
 		 log.Fatal(err)
 	 }
-	 all_files := FoldersList{Folders:make([]Folder, 0)}
-	 for _, obj := range files {
-		 f := Folder{Name:obj.Name(), Count:0}
-		 all_files.Folders = append(all_files.Folders, f)
+	 all_files := FileDataList{Files:make([]FileData, 0)}
+	 for _, obj := range files[start_index : end_index] {
+		 src_dat, err := ioutil.ReadFile(obj.Name())
+		 if err != nil {
+			 log.Fatal(err)
+		 }
+		 var dst_data []byte
+		 base64.StdEncoding.Encode(dst_data, src_dat)
+		 f := FileData{Name:obj.Name(), Data:dst_data}
+		 all_files.Files = append(all_files.Files, f)
 	 }
 	 b, err := json.Marshal(all_files)
 	 if err != nil {
