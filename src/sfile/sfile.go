07aff7b7387724a2435809c02f7cda9aff506cea
Joshua Matthews <jmatth93@gmail.com>
2017-05-04 23:53:40 -0500

    working on ReadSaveFile function

--- src/sfile/sfile.go
+++ src/sfile/sfile.go
@@ -1,7 +1,9 @@
 package sfile
 
 import (
-  "fmt"
+  "log"
+  "os"
+  "errors"
 )
 
 func intToBytes(uint64 n) (a, b, c, d byte) {
@@ -17,16 +19,49 @@ func bytesToInt(a, b, c, d byte) uint64 {
 }
 
 type HeaderFormat interface {
-  ParseHeader(data []byte)
+  ParseHeader(file *os.File, data []byte) error
 }
 
 type SaveFile struct {
 	Data      []byte
 	FileHash  []byte
-	Size 		  int64
+	Size 		  uint64
 	Header    HeaderFormat
 }
 
-func ReadSaveFile(file_name string) *SaveFile {
-
+func ReadSaveFile(file_name string, head HeaderFormat) (*SaveFile, error) {
+  sf := &SaveFile{Data:make([]byte, 0), FileHash:make([]byte, 0), Size:0, Header:head}
+  improper_file_format := errors.New("ERROR: File not formatted properly.")
+  file, err := os.Open(file_name)
+  if err != nil {
+    log.Fatal(err)
+    return nil, erros.New("ERROR: File could not be read.")
+  }
+  data := make([]byte, 8)
+  count, err := file.Read(data)
+  if count < 8 {
+    log.Fatal("File: \"" + file_name + "\" had less than 8 bytes.")
+    return nil, improper_file_format
+  } else if err != nil {
+    log.Fatal(err)
+    return nil, improper_file_format
+  }
+  if data[0:4] != []byte("SAVE") {
+    log.Fatal("File: \"" + file_name + "\" was not a proper save file.")
+    return nil, improper_file_format
+  }
+  head_size := bytesToInt(data[4:8]...)
+  if sf.Header != nil {
+    header_info := make([]byte, head_size)
+    count, err = file.ReadAt(header_info, count)
+    if err != nil {
+      log.Fatal(err)
+      return nil, errors.New("ERROR: Could not read header.")
+    }
+    err = sf.Header.ParseHeader(file, header_info)
+    if err != nil {
+      return nil, err
+    }
+  }
+  
 }
